a733c805ff7796a2da62e2d3fa3c3fa7a7b1e895
huangjie <huangjie@xiaohulu.com>
2022-08-08 13:13:08 +0800

    change

--- mock/api.ts
+++ mock/api.ts
@@ -1,16 +1,20 @@
 import mockjs from 'mockjs';
+let datalist = mockjs.mock({
+  code: 200,
+  msg: 'success',
+  'data|100': [
+    {
+      name: '@cname',
+      score: '@integer(0,100)',
+      city: '@city',
+      birth: '@date',
+      'id|+1': 1,
+    },
+  ],
+});
 export default {
-  'GET /classes/stulist': mockjs.mock({
-    code: 200,
-    msg: 'success',
-    'data|100': [
-      {
-        name: '@cname',
-        score: '@integer(0,100)',
-        city: '@city',
-        birth: '@date',
-        'id|+1': 1,
-      },
-    ],
-  }),
+  'GET /classes/stulist': datalist,
+  'DELETE /classes/stu': (req: any, res: any) => {
+    console.log(req.query);
+  },
 };
